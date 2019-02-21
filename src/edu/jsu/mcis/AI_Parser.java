7446e0a834df9385680f86177d756481b5496757
vinesmatthew <mvines1@stu.jsu.edu>
2019-02-21 15:41:52 -0600

    Added "AI_Parser"

---
+++ src/edu/jsu/mcis/AI_Parser.java
@@ -0,0 +1,14 @@
+package edu.jsu.mcis;
+
+import java.util.ArrayList;
+import java.util.regex.Pattern;
+import org.json.simple.JSONArray;
+
+public class AI_Parser {
+    
+    private JSONArray parser(ArrayList<Pattern> patterns) {
+        
+        JSONArray results = new JSONArray();
+        return results;
+    }
+}
