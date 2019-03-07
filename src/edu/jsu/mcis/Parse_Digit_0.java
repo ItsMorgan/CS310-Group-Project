5db9b07cc17a3a3b313531a8fb54132ac482fba4
vinesmatthew <mvines1@stu.jsu.edu>
2019-03-07 16:30:06 -0600

    Added lots of new stuff

--- src/edu/jsu/mcis/Parse_Digit_0.java
+++ src/edu/jsu/mcis/Parse_Digit_0.java
@@ -3,18 +3,26 @@ package edu.jsu.mcis;
 import java.util.ArrayList;
 import java.util.regex.*;
 
-public class Parse_Digit_0 {
-    
-    String code = "";
+public class Parse_Digit_0 extends Pattern_Parser {
 
-    ArrayList<Matcher> matches = new ArrayList();
-    
-    Pattern p00 = Pattern.compile("^00[0-9]{18}");         
-    Pattern p01 = Pattern.compile("^01[0-9]{14}");          
-    Pattern p02 = Pattern.compile("^02[0-9]{14}");
+    public Parse_Digit_0() {
+        
+        super();
+        
+        String code = super.code;
+        
+        matches = new ArrayList();
 
-    public Parse_Digit_0(String code) {
-        this.code = code;
+        Pattern p00 = Pattern.compile("^00[0-9]{18}");         
+        Pattern p01 = Pattern.compile("^01[0-9]{14}");          
+        Pattern p02 = Pattern.compile("^02[0-9]{14}");
+        
+        ArrayList<Pattern> patterns = new ArrayList<>();
+        patterns.add(p00);
+        patterns.add(p01);
+        patterns.add(p02);
+        
+        String aiAndData = super.parsePattern(patterns, code).group();
     }
     
     public String parse() {
@@ -23,6 +31,7 @@ public class Parse_Digit_0 {
     }
 
     public ArrayList<Matcher> getMatches() {
+        
         return matches;
     }
 
